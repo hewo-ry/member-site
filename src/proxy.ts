9e78b342aa5ce5c82e695de59a8bedf87d63a832
Ville Nupponen <ville.nupponen@hewo.fi>
2026-03-15 05:21:49 +0200

    Do not try to redirect POST request to keycloak

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,10 +3,25 @@ import { NextRequest, NextResponse } from 'next/server';
 
 import { auth } from '@/auth';
 
+import { ProblemDetails } from './lib/optire/types';
+
 export const proxy = async (request: NextRequest) => {
     const session = await auth.api.getSession({ headers: await headers() });
 
     if (session) return NextResponse.next();
+
+    if (request.method !== 'GET')
+        return NextResponse.json(
+            {
+                type: 'https://hewo.dev/member-site/Unauthorized',
+                title: 'Unauthorized',
+                status: 401,
+                detail: 'No session found',
+                instance: request.nextUrl.pathname,
+                properties: null,
+            } satisfies ProblemDetails,
+            { status: 401 },
+        );
 
     const response = await auth.api.signInWithOAuth2({
         body: {
